09f77d81dccec09399312eb977acfe752c670d10
kabachuha <artemkhrapov2001@yandex.ru>
2024-03-06 17:43:06 +0800

    fla is installable, so don't need to embed

--- flash_linear_attention/fla/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from fla.ops.triton import (fused_chunk_based, fused_chunk_gla,
-                                                   fused_chunk_retention)
-
-__all__ = ['fused_chunk_based', 'fused_chunk_gla', 'fused_chunk_retention']
-
-__version__ = '0.0.1'
--- flash_linear_attention/fla/ops/triton/rebased_fast/__init__.py
+++
@@ -1,4 +0,0 @@
-from .parallel import parallel_rebased
-
-
-__all__ = ["parallel_rebased"]
