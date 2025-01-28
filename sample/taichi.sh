37e4f0b3164bf88a65922ebd81a76d4a48c889c2
Xin Ma <maxin.cn@outlook.com>
2025-01-28 14:42:57 +1100

    Update taichi.sh

--- sample/taichi.sh
+++ sample/taichi.sh
@@ -2,6 +2,6 @@
 export CUDA_VISIBLE_DEVICES=7
 
 python sample/sample.py \
---config ./configs/ucf101/taichi_sample.yaml \
+--config ./configs/taichi/taichi_sample.yaml \
 --ckpt  ./share_ckpts/taichi-hd.pt \
 --save_video_path ./test
