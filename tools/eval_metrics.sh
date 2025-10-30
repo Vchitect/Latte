b27c24a17487e3c33ae4e92364521303d0f14845
Xin Ma <maxin.cn@outlook.com>
2025-10-31 10:04:13 +1100

    Update eval_metrics.sh

--- tools/eval_metrics.sh
+++ tools/eval_metrics.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 python tools/calc_metrics_for_dataset.py \
---real_data_path /path/to/real_data//images \
+--real_data_path /path/to/real_data/images \
 --fake_data_path /path/to/fake_data/images \
 --mirror 1 --gpus 1 --resolution 256 \
 --metrics fvd2048_16f  \
